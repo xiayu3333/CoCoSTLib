f03825ad30c7895d90927aec4ce1a69e20aabf24
Yu Xia <yu.xia@alumni.enac.fr>
2021-06-16 11:07:45 +0200

    initial commit

---
+++ slblocks.m
@@ -0,0 +1,12 @@
+function blkStruct = slblocks
+% This function specifies that the library 'mylib'
+% should appear in the Library Browser with the 
+% name 'My Library'
+
+    Browser.Library = 'mySTLLib';
+    % name of the library
+
+    Browser.Name = 'My STL Library';
+    % library name that appears in the Library Browser
+
+    blkStruct.Browser = Browser;
